01a1e618fddb29cf981f995387a9944765c410e3
Roberto <robertodotaruma@hotmail.com>
2022-11-30 11:50:40 +0100

    Métodos de inserir e alterar adicionados

--- class/Usuario.php
+++ class/Usuario.php
@@ -42,12 +42,7 @@ class Usuario {
 		//if (isset($results [0])){ mesma coisa
 		if (count($results) > 0){
 
-			$row = $results[0];
-
-			$this -> setIdusuario($row['idusuario']);
-			$this -> setDeslogin($row['deslogin']);
-			$this -> setDessenha($row['dessenha']);
-			$this -> setDtcadastro(new DateTime($row['dtcadastro']));
+			$this -> setData($results[0]);
 		}
 	}
 
@@ -79,16 +74,50 @@ class Usuario {
 		//if (isset($results [0])){ mesma coisa
 		if (count($results) > 0){
 
-			$row = $results[0];
+			$this -> setData($results[0]);
 
-			$this -> setIdusuario($row['idusuario']);
-			$this -> setDeslogin($row['deslogin']);
-			$this -> setDessenha($row['dessenha']);
-			$this -> setDtcadastro(new DateTime($row['dtcadastro']));
 		} else {
 
 			throw new Exception("Login e/ou senha inválidos.");
 		}
+	}
+
+	public function setData($data){
+
+		$this -> setIdusuario($data['idusuario']);
+		$this -> setDeslogin($data['deslogin']);
+		$this -> setDessenha($data['dessenha']);
+		$this -> setDtcadastro(new DateTime($data['dtcadastro']));
+
+	}
+
+	public function insert(){
+
+		$sql = new Banco();
+
+		  $results = $sql -> select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+
+			':LOGIN' => $this -> getDeslogin(),
+			':PASSWORD' => $this -> getDessenha()
+		));
+
+		if (count($results) > 0 ){
+			$this -> setData($results[0]);
+		}
+	}
+
+	public function update($login ="", $password = ""){
+
+		$this -> setDeslogin ($login);
+		$this -> setDessenha ($password);
+
+		$sql = new Banco();
+
+		$sql -> run("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+			':LOGIN' => $this -> getDeslogin(),
+			':PASSWORD' => $this -> getDessenha(),
+			':ID' => $this -> getIdusuario()
+		));
 	}
 
 	public function __toString(){
--- index.php
+++ index.php
@@ -5,33 +5,39 @@ require_once("config.php");
 /*$sql = new Banco();
 
 $usuarios = $sql -> select("SELECT * FROM tb_usuarios");
-
 //foreach ($usuarios)
-
 //print_r($usuarios);
-
-
 echo json_encode($usuarios);*/
-// carrega um usuário
-/*$saldanha = new Usuario();
-$saldanha -> loadById(5);
-echo $saldanha; */
 
-//carrega uma lista de usuários
-/*$lista = Usuario::getList();
+//Carrega um usuário
+//$saldanha = new Usuario();
+//$saldanha -> loadById(5);
+//echo $saldanha;
 
-echo json_encode($lista); */
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-/*// Carrega uma lista de usuários buscando pelo login
-$busca = Usuario::search("er");
-
-echo json_encode($busca); */
+// Carrega uma lista de usuários buscando pelo login
+//$busca = Usuario::search("er");
+//echo json_encode($busca); 
 
 //Carrega um usuário usando o login e a senha
-$usuario = new Usuario();
-$usuario -> login("saldanha","123456789");
+//$usuario = new Usuario();
+//$usuario -> login("saldanha","123456789");
+//echo $usuario;
 
-echo $usuario;
+//Insert de um usuário novo
+//$user = new Usuario();
+//$user -> setDeslogin("dauphine");
+//$user -> setDessenha("fianne2002");
+//$user -> insert();
+//echo $user;
 
+//Update em um usuário
+$user = new Usuario();
+$user -> loadById(15);
+$user -> update("dianedark", "fianne155!");
+echo $user;
 
 ?>
